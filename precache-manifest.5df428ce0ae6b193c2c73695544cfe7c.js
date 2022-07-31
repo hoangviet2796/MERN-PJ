3523a3a666151ebca24bb80a35a42ee51eceb0f4
Viet <nguyenkieuhoangviet@gmail.com>
2022-07-31 22:25:26 +0700

    Updates

---
+++ precache-manifest.5df428ce0ae6b193c2c73695544cfe7c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9de3475878fd78f2bb5d68528cf136",
+    "url": "/MERN-PJ/index.html"
+  },
+  {
+    "revision": "17e12195e81b2e77b032",
+    "url": "/MERN-PJ/static/css/4.73c71b56.chunk.css"
+  },
+  {
+    "revision": "c75636941d00900412a6",
+    "url": "/MERN-PJ/static/css/5.9e7571ea.chunk.css"
+  },
+  {
+    "revision": "8ec4b3ef17d38fbceb49",
+    "url": "/MERN-PJ/static/css/6.530233a9.chunk.css"
+  },
+  {
+    "revision": "129234ed39573db4576c",
+    "url": "/MERN-PJ/static/css/7.530233a9.chunk.css"
+  },
+  {
+    "revision": "3971f97af4461c886cbf",
+    "url": "/MERN-PJ/static/css/main.ea6da5ed.chunk.css"
+  },
+  {
+    "revision": "bef9b902a03dfcb098d6",
+    "url": "/MERN-PJ/static/js/2.4ce01c4b.chunk.js"
+  },
+  {
+    "revision": "e400443472c3546e7b59",
+    "url": "/MERN-PJ/static/js/3.5f37f3b9.chunk.js"
+  },
+  {
+    "revision": "17e12195e81b2e77b032",
+    "url": "/MERN-PJ/static/js/4.6606e354.chunk.js"
+  },
+  {
+    "revision": "c75636941d00900412a6",
+    "url": "/MERN-PJ/static/js/5.a97c2323.chunk.js"
+  },
+  {
+    "revision": "8ec4b3ef17d38fbceb49",
+    "url": "/MERN-PJ/static/js/6.bf1f5283.chunk.js"
+  },
+  {
+    "revision": "129234ed39573db4576c",
+    "url": "/MERN-PJ/static/js/7.d7bda359.chunk.js"
+  },
+  {
+    "revision": "5f3131b460b27bdb2b0c",
+    "url": "/MERN-PJ/static/js/8.80b714c8.chunk.js"
+  },
+  {
+    "revision": "3971f97af4461c886cbf",
+    "url": "/MERN-PJ/static/js/main.350afc39.chunk.js"
+  },
+  {
+    "revision": "7f889b7484bb62a508c6",
+    "url": "/MERN-PJ/static/js/runtime-main.ceb85c71.js"
+  }
+]);
